11ffbebdc291045688824dc3231adc563e5e8543
exceldeo <excel.deo99@gmail.com>
2022-03-24 15:43:49 +0700

    pemasangan template

---
+++ presensi_online_web/resources/views/auth/login.blade.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign in &bullet; myITS E-Klenik</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="https://my.its.ac.id/assets/media/favicons/favicon-web.png">
+    <link rel="stylesheet" type="text/css" href="https://my.its.ac.id/assets/css/its-login.css">
+    <link rel="stylesheet" type="text/css" href="https://my.its.ac.id/assets/plugins/nprogress/nprogress.css">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<style>
+		.alert-error {
+			color: #f1c40f !important;
+			padding: 10px;
+			border-radius: 3px;
+			text-align: center;
+		}
+
+		.btn-eye {
+			background-color: transparent;
+			padding: 5px;
+			border: none;
+		}
+
+		.btn-eye:hover {
+			cursor: pointer;
+		}
+
+	</style>
+
+</head>
+<body>
+	<div class="wrapper">
+		<div class="box">
+			<img srcset="https://my.its.ac.id/assets/media/img/@2x/logo.png 2x, https://my.its.ac.id/assets/media/img/logo.png 1x"
+					src="https://my.its.ac.id/assets/media/img/logo.png" alt="Logo ITS" class="logo">
+			<div class="description">
+				<!-- <img srcset="https://my.its.ac.id/assets/media/img/@2x/myits-sso-white.png 2x, https://my.its.ac.id/assets/media/img/myits-sso-white.png 1x" src="https://my.its.ac.id/assets/media/img/myits-sso-white.png" alt="myITS" class="myits"> -->
+			</div>
+
+
+		    <form id="form-login" method="post" action="{{ route('login') }}" autocomplete="off">
+		        @csrf
+		        <div class="text-orange" style="min-height: 20px; font-weight: bold; margin-bottom: 15px; text-align: center">
+					<small class="notification_caps" style="display: none;">Caps lock is on!</small>
+					<small class="my_its_id_required" style="display: none;">Email is required</small>
+				</div>
+
+                @error('email')
+                    <div class="text-orange" style="font-weight: bold; margin-bottom: 15px;">
+                        <small>{{ $errors->first('email') }}</small>
+                    </div>
+                @enderror
+                <div id="inputusername" class="inputbox">
+					<input type="text" id="username" name="email" value="" required autocomplete="off">
+					<label for="">Email</label>
+		        </div>
+
+				<div id="inputpassword" class="inputbox" >
+					<input type="password" name="password" id="password" required autocomplete="off">
+					<label for="">Password</label>
+				</div>
+				<div class="inputbox">
+		            <button id="login" type="submit" class="btn btn-block bg-orange">Sign in</button>
+				</div>
+			</form>
+
+		</div>
+		<footer class="m-t-30">&copy; 2022 Institut Teknologi Sepuluh Nopember. <em>v2.2.6</em></footer>
+	</div>
+
+	<script src="https://my.its.ac.id/assets/js/core/jquery.min.js"></script>
+    <script src="https://my.its.ac.id/assets/plugins/nprogress/nprogress.js"></script>
+
+
+<script src="https://my.its.ac.id/assets/js/common.min.js"></script>
+<script src="https://my.its.ac.id/assets/js/jsencrypt.min.js"></script>
+
+<script>
+
+const autofillContent = `"${String.fromCharCode(0xFEFF)}"`;
+function checkAutofill(input) {
+    if (!input.value) {
+        const style = window.getComputedStyle(input);
+        if (style.content !== autofillContent)
+            return false;
+    }
+    // the autofill was detected
+    username.classList.add('filled');
+    password.classList.add('filled');
+    return true;
+}
+
+const username = document.querySelector("input[type=text]");
+const password = document.querySelector("input[type=password]");
+
+$("#inputpassword").on('keydown', function (e) {
+    if (e.keyCode == 13) {
+		submitForm();
+    }
+});
+
+function submitForm() {
+	var encrypted = encrypt();
+	$("#encrypted").val(encrypted);
+
+	$("#form-login").submit();
+}
+
+function encrypt() {
+	var encryptor = new JSEncrypt();
+	encryptor.setPublicKey($('#pubkey').val());
+	var username = $("#username").val();
+	var password = $("#password").val();
+	var device_method = $("#device_method").val();
+	var password_state = $("#password_state").val();
+	var plaintext = { u : username, p : password, dm: device_method, ps: password_state};
+	var myJSON = JSON.stringify(plaintext);
+	var encrypted = encryptor.encrypt(myJSON);
+
+	return encrypted;
+}
+
+$(function() {
+	$(window).keydown(function(event){
+		if(event.keyCode == 13) {
+			event.preventDefault();
+		}
+	});
+
+	loader = `<i class="fa fa-spinner fa-pulse"></i>`;
+	button = `Lanjutkan`;
+	$("#login").click(function(){
+		submitForm();
+	});
+});
+</script>
+
+</body>
+</html>
---
+++ presensi_online_web/resources/views/dashboard/layouts/master.blade.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en" class="no-focus">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+	<title>@yield('title')</title>
+
+	<meta name="description" content="SI Presensi">
+	<meta name="author" content="DPTSI ITS">
+	<meta name="robots" content="noindex, nofollow">
+
+	<!-- Icons -->
+	<!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+	<link rel="shortcut icon" href="https://presensi.its.ac.id/assets/media/favicons/favicon-web.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="https://presensi.its.ac.id/assets/media/favicons/favicon-web.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="https://presensi.its.ac.id/assets/media/favicons/favicon-web.png">
+	<!-- END Icons -->
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="{{asset('template')}}/js/plugins/select2/css/select2.min.css">
+	<!-- <link rel="stylesheet" href="{{asset('template')}}/js/plugins/clockpicker/dist/jquery-clockpicker.min.css"> -->
+	<link rel="stylesheet" href="{{asset('template')}}/js/plugins/sweetalert2/sweetalert2.min.css">
+	<link rel="stylesheet" href="{{asset('template')}}/js/plugins/magnific-popup/magnific-popup.css">
+	<link rel="stylesheet" href="{{asset('template')}}/js/plugins/chartjs/Chart.min.css">
+	<link rel="stylesheet" href="{{asset('template')}}/js/plugins/datatables/dataTables.bootstrap4.css">
+	<!-- Fonts and Codebase framework -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,400i,600,700">
+	<link rel="stylesheet" id="css-main" href="{{asset('template')}}/css/codebase.min.css">
+	<link rel="stylesheet" href="{{asset('template')}}/css/created/style.css">
+    <link rel="stylesheet" href="{{asset('template')}}/css/its/its.css">
+
+    <style>
+        h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {
+            font-family: "Muli", -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+            line-height: 1.2;
+        }
+    </style>
+
+	<!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+	<!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/flat.min.css"> -->
+    <!-- END Stylesheets -->
+    @yield('css')
+</head>
+<body>
+<div id="page-container" class="enable-page-overlay side-scroll page-header-modern main-content-boxed">
+	<!-- Header -->
+	<header id="page-header">
+		<!-- Header Content -->
+		<div class="content-header">
+			<!-- Left Section -->
+			<div class="content-header-section">
+				<!-- Toggle Sidebar -->
+				<div class="content-header-item">
+                    <a class="font-w700" href="#">
+						{{-- <img src="https://presensi.its.ac.id/assets/media/img/myits-academics.png" alt="Logo ITS" height="40px" class="mr-5"> --}}
+						<span class="font-size-l text-its-secondary text-uppercase">E-Klenik</span>
+					</a>
+				</div>
+				<!-- END Toggle Sidebar -->
+			</div>
+			<!-- END Left Section -->
+
+			<!-- Right Section -->
+			<div class="content-header-section">
+                <!-- User Dropdown -->
+				<div class="btn-group" role="group">
+					<button type="button" class="btn btn-rounded btn-dual-secondary" id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<i class="fa fa-user d-sm-none"></i>
+							<span class="d-none d-sm-inline-block">
+                                <i class="fa fa-user"></i>
+								<span>{{ Auth::user()->username }}</span>
+							</span>
+						<i class="fa fa-angle-down ml-5"></i>
+					</button>
+					<div class="dropdown-menu dropdown-menu-right min-width-200" aria-labelledby="page-header-user-dropdown">
+						<a class="dropdown-item" href="{{route('logout')}}">
+							<i class="fa ffa-arrow-circle-left mr-5"></i> Keluar
+						</a>
+					</div>
+				</div>
+				<!-- END User Dropdown -->
+			</div>
+			<!-- END Right Section -->
+		</div>
+		<!-- END Header Content -->
+
+		<!-- Header Loader -->
+		<div id="page-header-loader" class="overlay-header bg-primary">
+			<div class="content-header content-header-fullrow text-center">
+				<div class="content-header-item">
+					<i class="fa fa-sun-o fa-spin text-white"></i>
+				</div>
+			</div>
+		</div>
+		<!-- END Header Loader -->
+	</header>
+	<!-- END Header -->
+
+    <!-- Main Container -->
+    <main id="main-container">
+    @yield('breadcrumb')
+    @yield('main')
+    </main>
+	<!-- END Main Container -->
+
+	<!-- Footer -->
+	<footer id="page-footer" class="opacity-0">
+		<div class="content py-20 font-size-xs clearfix">
+			<div class="row align-items-center">
+				<div class="col-6">Copyright &copy; <span class="js-year-copy"></span> Institut Teknologi Sepuluh Nopember</div>
+				<div class="col-6 text-right">
+					<img src="https://presensi.its.ac.id/assets/media/img/logo-its-biru.png" height="40">
+					<img src="https://presensi.its.ac.id/assets/media/img/logo-its-semangat-baru.png" height="40px">
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- END Footer -->
+</div>
+<!-- END Page Container -->
+
+<script src="{{asset('template')}}/js/codebase.core.min.js"></script>
+<script src="{{asset('template')}}/js/codebase.app.min.js"></script>
+<script src="{{asset('template')}}/js/plugins/select2/js/select2.full.min.js"></script>
+<script src="{{asset('template')}}/js/plugins/masked-inputs/jquery.maskedinput.min.js"></script>
+<script src="{{asset('template')}}/js/plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{asset('template')}}/js/plugins/sweetalert2/sweetalert2.min.js"></script>
+<script src="{{asset('template')}}/js/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="{{asset('template')}}/js/plugins/chartjs/Chart.min.js"></script>
+<script>
+	$('.input-time').toArray().forEach(function (field) {
+		new Cleave(field, {
+			time: true,
+			timePattern: ['h', 'm']
+		});
+	});
+
+	jQuery(function () {
+		Codebase.helpers(['datepicker', 'colorpicker', 'maxlength', 'select2', 'masked-inputs', 'rangeslider', 'tags-inputs', 'slimscroll', 'magnific-popup']);
+	});
+</script>
+@yield('js')
+</body>
+</html>
