30784300a1d3205df1d9187b309e5f7cc17237e9
exceldeo <excel.deo99@gmail.com>
2022-05-17 19:03:16 +0700

    mengubah logo

--- presensi_online_mobile/lib/main.dart
+++ presensi_online_mobile/lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyITS Academy',
+      title: 'MyITS Presensi',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: ColorResources.COLOR_PRIMARY,
